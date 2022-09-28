2689852f3faf8017d625b27e512d9056aa44cfbd
Divyanshu Singh <dsinecos@gmail.com>
2022-09-28 11:35:10 +0530

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Steps
+1. Install using cmake - https://github.com/google/brotli#autotools-style-cmake
+	a) Run ../configure-cmake --disable-debug (to generate Release Makefile)
+  b) For local I wanted to install in `~/.local/` and not `usr/local` as it requires sudo permissions. For this I needed to update the file cmake_install.cmake file for the variable CMAKE_INSTALL_PREFIX and multiple other updates where it attempted to install or copy files to `/usr/local`
+
+To run this example
+CGO_CFLAGS='-I /home/ds/.local/include' CGO_LDFLAGS='-L /home/ds/.local/lib' go run main.go
--- main.go
+++ main.go
@@ -6,14 +6,6 @@ import (
 	"github.com/google/brotli/go/cbrotli"
 )
 
-// Steps
-// 1. Install using cmake - https://github.com/google/brotli#autotools-style-cmake
-// 	a) Run ../configure-cmake --disable-debug (to generate Release Makefile)
-//  b) For local I wanted to install in `~/.local/` and not `usr/local` as it requires sudo permissions. For this I needed to update the file cmake_install.cmake file for the variable CMAKE_INSTALL_PREFIX
-
-// To run this example
-// CGO_CFLAGS='-I /home/ds/.local/include' CGO_LDFLAGS='-L /home/ds/.local/lib' go run main.go
-
 func main() {
 	fmt.Println("Hello World!")
 
